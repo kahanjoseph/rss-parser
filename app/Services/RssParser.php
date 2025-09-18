b290902a805d66cad891bbdd8fe9f5c10cece42e
Joseph Kahan <kahanjoseph@gmail.com>
2025-09-18 11:58:30 -0400

    Attempt at creating own rss parser

--- app/Services/RssParser.php
+++ app/Services/RssParser.php
@@ -19,11 +19,24 @@ class RssParser{
             throw new \Exception("Failed to fetch RSS feed: " . $response->status());
         }
 
-        $feed = simplexml_load_string($response->body());
-        if(!$feed){
-            throw new \Exception("Failed to parse RSS feed");
+        preg_match_all('/<item>(.*?)<\/item>/s', $response->body(), $matches);
+
+        $results = [];
+
+        foreach ($matches[1] as $key => $value) {
+            preg_match('/<title>(.*?)<\/title>/s', $value, $titleMatch);
+            preg_match('/<description>(.*?)<\/description>/s', $value, $descriptionMatch);
+            preg_match('/<link>(.*?)<\/link>/s', $value, $linkMatch);
+            preg_match('/<pubDate>(.*?)<\/pubDate>/s', $value, $pubDateMatch);
+
+            $results[] = [
+                'title' => $titleMatch[1],
+                'description' => $descriptionMatch[1],
+                'link' => $linkMatch[1],
+                'pubDate' => $pubDateMatch[1]
+            ];
         }
-        return $feed;
+        return $results;
     }
 
     public function feed()
@@ -35,19 +48,19 @@ class RssParser{
     private function extractGoogleItem($item) : array {
 
         return [
-            'description' => preg_replace('/(google)/i', "<span class=\"text-red-500\">$1</span>", htmlspecialchars($item->description)),
-            'title' => preg_replace('/(google)/i', "<span class=\"text-red-500\">$1</span>", htmlspecialchars($item->title)),
-            'link' => $item->link,
-            'date' => $item->pubDate
+            'description' => preg_replace('/(google)/i', "<span class=\"text-red-500\">$1</span>", htmlspecialchars($item['description'])),
+            'title' => preg_replace('/(google)/i', "<span class=\"text-red-500\">$1</span>", htmlspecialchars($item['title'])),
+            'link' => $item['link'],
+            'date' => $item['pubDate']
         ];
     }
 
     public function getGoogleCount(){
         $count = 0;
         $formattedData = [];
-        foreach ($this->feed->channel->item as $item) {
-            if(str_contains(strtolower($item->description . $item->title), 'google')){
-                $count += substr_count(strtolower($item->description . $item->title), 'google');
+        foreach ($this->feed as $item) {
+            if(str_contains(strtolower($item['description'] . $item['title']), 'google')){
+                $count += substr_count(strtolower($item['description'] . $item['title']), 'google');
                 $formattedData[] = $this->extractGoogleItem($item);
             }
         }
